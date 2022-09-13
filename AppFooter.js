1303cb6cbd543a42874df8a35df56e9f052cef6f
Vaishnavi Shinde <83237854+Vaishnavi2401@users.noreply.github.com>
2022-09-13 12:37:42 +0530

    Delete AppFooter.js

--- AppFooter.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-export function AppFooter(props){
-    return(
-        <div class= 'App-footer'>
-            <p><i>&copy; All Rights reserved under {props.company}-{props.year}</i></p>
-        </div>
-    )
-}
